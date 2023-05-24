bd390a8bc76198ce32ad08d04bb6ec3e483c29fa
jm448365 <jm448365@students.mimuw.edu.pl>
2023-05-24 15:42:26 +0200

    Dodane male dane testowe + test funkcjonalnosci Wsysarki w mainie

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         Wsysara wsys = new Wsysara();
+        wsys.wessijPlik("./dane");
+        HashMap<String,Integer> slowa = wsys.getWords();
+        for(HashMap.Entry<String, Integer> slowo : slowa.entrySet()) {
+            System.out.println(slowo.getKey() + " : " + slowo.getValue());
+        }
     }
 }
