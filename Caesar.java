d38750275f00fbe785fb1b7712af109f69ded28b
c00kiepreferences <paulinakubera888@gmail.com>
2023-05-24 15:44:49 +0200

    Dodaje upo Caesara bo nie bylo konstruktora

---
+++ Caesar.java
@@ -0,0 +1,15 @@
+public class Caesar extends Cipher {
+
+    private int shift;
+
+    public Caesar (int shift){
+        this.shift = shift;
+    }
+
+    @Override
+    public String encode(String s) {
+        String ret;
+        char letter;
+        return "";
+    }
+}
