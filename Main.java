978a6ee4edbb29d71737cd46a4bcfd3d252289d4
Wiktoria Siekierska <wiktoria@MacBook-Pro-Wiktoria.local>
2023-05-24 15:55:13 +0200

    Dodanie wypisywania w mainie

--- Main.java
+++ Main.java
@@ -1,16 +1,26 @@
+import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
+        int n;
         Wysysara moj_tekst=new Wysysara();
         moj_tekst.wessijPlik(dupsko);
         new HashMap<String String> slowa;
         slowa = moj_tekst.get_words();
+        System.out.println("Wejściowe słowa:");
         for(HashMap<String String> word: slowa){
-            System.out.print(word);
-            System.out.print(" ");
+            System.out.println(word.getValue());
         }
-        Cipher cezar=new Cipher(2);
-        Cipher mors=new Cipher(2);
-
-        moj_tekst.getEncodedWords(Cipher cezar);
+        Caesar cezar=new Caesar(n);
+        CipherMors mors=new CipherMors();
+        System.out.println("Słowa zaszyfrowane szyfrem Cezara o kluczu " +n ":");
+        words=moj_tekst.getEncodedWords(Caesar cezar);
+        for(HashMap<String String> word: slowa){
+            System.out.println(word.getValue());
+        }
+        System.out.println("Słowa zaszyfrowane alfabetem morsa:");
+        words=moj_tekst.getEncodedWords(Caesar cezar);
+        for(HashMap<String String> word: slowa){
+            System.out.println(word.getValue());
+        }
     }
 }
