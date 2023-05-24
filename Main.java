cb212288320317dab9604650f798a36bd1404ef3
Wiktoria Siekierska <wiktoria@MacBook-Pro-Wiktoria.local>
2023-05-24 17:24:23 +0200

    Zmiana danych, ostateczne poprawki

--- Main.java
+++ Main.java
@@ -1,26 +1,26 @@
 import java.util.HashMap;
 public class Main {
     public static void main(String[] args) throws Exception{
-        int n;
+        int n=42;
         Wsysara moj_tekst=new Wsysara();
         moj_tekst.wessijPlik("dane");
-        HashMap<String, Integer> slowa= new HashMap<String, Integer>;
+        HashMap<String, Integer> slowa= new HashMap<>();
         slowa = moj_tekst.getWords();
         System.out.println("Wejściowe słowa:");
-        for(HashMap<String, Integer> word: slowa){
-            System.out.println(word.getValue());
+        for(HashMap.Entry<String, Integer> word: slowa.entrySet()){
+            System.out.println(word.getKey());
         }
-        Caesar cezar=new Caesar(n);
+        CipherCaesar cezar=new CipherCaesar(n);
         CipherMorse mors=new CipherMorse();
-        System.out.println("Słowa zaszyfrowane szyfrem Cezara o kluczu " +n ":");
-        HashMap<String, Integer> words= new HashMap<String, Integer>;
-        words=moj_tekst.getEncodedWords(Caesar cezar);
-        for(HashMap<String, String> word: words){
+        System.out.println("Słowa zaszyfrowane szyfrem Cezara o kluczu " +n+ ":");
+        HashMap<String, String> words= new HashMap<>();
+        words=moj_tekst.getEncodedWords(cezar);
+        for(HashMap.Entry<String, String> word: words.entrySet()){
             System.out.println(word.getValue());
         }
         System.out.println("Słowa zaszyfrowane alfabetem morsa:");
-        words=moj_tekst.getEncodedWords(CipherMorse mors);
-        for(HashMap<String, String> word: words){
+        words=moj_tekst.getEncodedWords(mors);
+        for(HashMap.Entry<String, String> word: words.entrySet()){
             System.out.println(word.getValue());
         }
     }
